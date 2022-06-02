9720fb706f2830cc8de946f52706992cd2d5e4d0
unknown <felippe_amaral@live.com>
2022-06-02 14:38:21 -0300

    Principais verbos HTTP

---
+++ 03 RestWithSpringBootUdemy Scaffold Verbos/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,59 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person create(Person person) {
+		//Persirtir na base de dados
+		return person;
+	}
+	
+	public Person update(Person person) {
+		//Persirtir na base de dados
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Felippe");
+		person.setLastName("Amaral");
+		person.setAddress("Rua Heraclito Graça - Rio de Janeiro - Brasil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in - Brasil " + i);
+		person.setGender("Male");
+		return person;
+	}
+}
