1f9380fba16e977ca1e3bc9031dcd25913c4f7cb
unknown <felippe_amaral@live.com>
2022-06-15 01:59:27 -0300

    Suporte a JWT e Spring Security

---
+++ 13 RestWithSpringBootUdemy JWT e Spring Security/src/main/java/br/com/erudio/controller/PersonController.java
@@ -0,0 +1,73 @@
+package br.com.erudio.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.services.PersonServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Person Endpoint", description = "Endpoint para Persons", tags = {"PersonEndpoint"})
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController { 
+	
+	@Autowired
+	private PersonServices services;
+	
+	@ApiOperation(value = "Find all people recorded")
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public List<PersonVO> findAll() {
+		List<PersonVO> persons = services.findAll();
+		persons.stream()
+				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
+	}
+	
+	@ApiOperation(value = "Find a unique people recorded")
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO findById(@PathVariable("id") Long id) {
+		PersonVO personVO = services.findById(id);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return personVO;
+	}
+	
+	@ApiOperation(value = "Save one record of people")
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" },
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO create(@RequestBody PersonVO person) {
+		PersonVO personVO = services.create(person);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+	
+	@ApiOperation(value = "Update people recorded")
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" },
+			consumes = { "application/json", "application/xml", "application/x-yaml" })
+	public PersonVO update(@RequestBody PersonVO person) {
+		PersonVO personVO = services.update(person);
+		personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+		return personVO;
+	}
+	
+	@ApiOperation(value = "Delete people recorded")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		services.delete(id);
+		return ResponseEntity.ok().build();
+	}
+}
