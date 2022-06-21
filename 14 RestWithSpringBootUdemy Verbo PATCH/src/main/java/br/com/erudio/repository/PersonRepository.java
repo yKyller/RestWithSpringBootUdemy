e8602f48484f331fd50b2cdd5c4a1f3ffaca008c
yKyller <felippe_amaral@live.com>
2022-06-21 18:59:04 -0300

    Verbo PATCH

---
+++ 14 RestWithSpringBootUdemy Verbo PATCH/src/main/java/br/com/erudio/repository/PersonRepository.java
@@ -0,0 +1,21 @@
+package br.com.erudio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.erudio.data.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+	@Modifying
+	@Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
+	void disablePersons(@Param("id") Long id);
+	
+	@Modifying
+	@Query("UPDATE Person p SET p.enabled = true WHERE p.id =:id")
+	void enablePersons(@Param("id") Long id);
+}
