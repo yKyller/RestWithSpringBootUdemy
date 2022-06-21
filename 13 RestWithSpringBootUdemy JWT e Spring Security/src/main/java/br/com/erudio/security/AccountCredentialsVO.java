551832f7a5f28934151ebb25ff5cfcb1e980844f
yKyller <felippe_amaral@live.com>
2022-06-21 18:56:15 -0300

    Ajustes no espaçamento do arquivo

--- 13 RestWithSpringBootUdemy JWT e Spring Security/src/main/java/br/com/erudio/security/AccountCredentialsVO.java
+++ 13 RestWithSpringBootUdemy JWT e Spring Security/src/main/java/br/com/erudio/security/AccountCredentialsVO.java
@@ -13,19 +13,24 @@ public class AccountCredentialsVO implements Serializable{
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(password, username);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
