bce96b21235180064e6a86a8357b53e6174faad8
yKyller <felippe_amaral@live.com>
2022-06-30 10:38:38 -0300

    Projeto criado a partir de um Archetype maven

---
+++ 20 RestWithSpringBootUdemy Generated From Archetype/rest-from-archetype/src/main/java/br/com/kyller/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package br.com.kyller.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.kyller.data.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+	@Query("SELECT b FROM Book b WHERE b.title LIKE LOWER(CONCAT ('%', :title, '%'))")
+	Page<Book> findBookByTitle(@Param("title") String title, Pageable pageable);
+}
